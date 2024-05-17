4bb78699950f6eedfcec789766a16359ac789763
RubenFern <rubenferval@hotmail.com>
2024-05-17 16:12:49 +0200

    colors added

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -1,29 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
-  theme: {
-    extend: {
-        backgroundColor: {
-            'dark-mode': '#171717',
-            'bg-navbar': '#2F2F2F',
+    darkMode: 'selector',
+    content: ["./src/**/*.{html,ts}"],
+    theme: {
+        extend: {
+            backgroundColor: {
+                "dark-mode": "#171717",
+                "light-mode": "#B5C0D0",
+            },
+            colors: {
+                transparent: "transparent",
+                current: "currentColor",
+                white: "#ffffff",
+                purple: "#3f3cbb",
+                midnight: "#121063",
+                metal: "#565584",
+                tahiti: "#3ab7bf",
+                silver: "#ecebff",
+                "bubble-gum": "#ff77e9",
+                bermuda: "#78dcca",
+                red: "#CC1212",
+                light: "#3F51B5"
+            },
         },
-        colors: {
-            transparent: 'transparent',
-            current: 'currentColor',
-            'white': '#ffffff',
-            'purple': '#3f3cbb',
-            'midnight': '#121063',
-            'metal': '#565584',
-            'tahiti': '#3ab7bf',
-            'silver': '#ecebff',
-            'bubble-gum': '#ff77e9',
-            'bermuda': '#78dcca',
-            'red': '#CC1212',
-          },
     },
-  },
-  plugins: [],
-}
-
+    plugins: [],
+};
